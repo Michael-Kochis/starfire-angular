1325ba6a4e9220a94a489477f806bad9bc503ff0
Michael-Kochis <rhishisikk@yahoo.com>
2026-02-17 09:32:54 -0600

    favicon.ico now working

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('starfire-angular');
+  protected readonly title = signal('Starfire Online');
 }
